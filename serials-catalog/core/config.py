7b8b43fba4eae2621da626f3d6975f70491f1e5a
Krushovice <kickstar69@yandex.ru>
2025-03-23 20:34:25 +1000

    update config

--- serials-catalog/core/config.py
+++ serials-catalog/core/config.py
@@ -1,21 +1,66 @@
+import logging
 from pathlib import Path
+from typing import Literal
 
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
+
+LOG_DEFAULT_FORMAT = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DB_PATH = BASE_DIR / "db.sqlite3"
 
-class DBConfig(BaseModel):
-    url = BASE_DIR / "db.sqlite3"
+
+class LoggingConfig(BaseModel):
+    log_level: Literal[
+        "debug",
+        "info",
+        "warning",
+        "error",
+        "critical",
+    ] = "info"
+    log_format: str = LOG_DEFAULT_FORMAT
+
+    @property
+    def log_level_value(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level.upper()]
+
+
+class DbConfig(BaseModel):
+    url = f"sqlite+aiosqlite:///{DB_PATH}"
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
+        env_prefix="MOVIE_CATALOG__",
+        env_nested_delimiter="__",
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
     )
+    logging: LoggingConfig = LoggingConfig()
+    db: DbConfig
 
-    db: DBConfig = DBConfig()
 
-
+# noinspection PyArgumentList
 settings = Settings()
